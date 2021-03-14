68efdaa8a8c74b1558d375480b0c4ec4f1417890
GL Zhao <glzhao@linkedin.com>
2021-03-13 23:57:05 -0600

    Initial commit

---
+++ Assets/Scripts/MainGameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameManager : MonoBehaviour
+{
+    public GameObject hexGridPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int rowIdx = 0; rowIdx < 5; rowIdx++) {
+            for (int colIdx = 0; colIdx < 5; colIdx++) {
+                GameObject grid = Instantiate(hexGridPrefab, new Vector3(colIdx * 1.732f + rowIdx % 2 * 0.866f, 0, rowIdx * 1.5f), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
